15c8be5b85d0c46b3cf7aeea03083d6bb01a2160
Alex Ung <alexung.ucsb@gmail.com>
2017-04-19 20:45:35 -0700

    insert photos and center

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import car from '../photos/cars1.jpeg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      photo1: '../photos/cars1.jpeg'
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container'>
+        <h1>Hello World</h1>
+        <img src={car} alt='awesome' className='annotate-image' />
       </div>
     );
   }
