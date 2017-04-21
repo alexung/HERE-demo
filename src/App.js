12bd4b76b63daa7b03fb5f45551844e337261dc5
Alex Ung <alexung.ucsb@gmail.com>
2017-04-20 17:10:55 -0700

    react-konva

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import car from '../photos/cars1.jpeg';
+import Konva from 'konva';
+import {Layer, Rect, Stage, Group} from 'react-konva';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      photo1: '../photos/cars1.jpeg'
+      photo1: '../photos/cars1.jpeg',
+      color: 'green'
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
   }
 
   render() {
@@ -16,6 +26,16 @@ class App extends Component {
       <div className='container'>
         <h1>Hello World</h1>
         <img src={car} alt='awesome' className='annotate-image' />
+        <Stage width={700} height={700}>
+          <Layer>
+            <Rect
+                x={10} y={10} width={50} height={50}
+                fill={this.state.color}
+                shadowBlur={10}
+                onClick={this.handleClick}
+            />
+          </Layer>
+        </Stage>
       </div>
     );
   }
