ab2c7f41de376fc5c715996c989fe8391d675757
Alex Ung <alexung.ucsb@gmail.com>
2017-05-18 15:38:56 -0700

    ejected app, and began react d3 tutorial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import car from '../photos/cars1.jpeg';
-import Konva from 'konva';
-import {Layer, Rect, Stage, Line, Circle} from 'react-konva';
+import {BarChart, Chart, LineChart} from 'react-d3-basic';
+import * as d3 from 'd3';
+import ProgressArc from './ProgressArc';
 import './App.css';
 
 class App extends Component {
@@ -11,47 +12,74 @@ class App extends Component {
     this.state = {
       photo1: '../photos/cars1.jpeg',
       color: 'green',
-      lineColor: 'red'
+      lineColor: 'red',
+      garbage: []
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleLineClick = this.handleLineClick.bind(this);
   }
 
   handleClick() {
-    this.setState({ color: Konva.Util.getRandomColor() });
+    // this.setState({ color: Konva.Util.getRandomColor() });
   }
 
   handleLineClick() {
-    this.setState({ lineColor: Konva.Util.getRandomColor() })
+    // this.setState({ lineColor: Konva.Util.getRandomColor() })
   }
 
   render() {
+    let chartData = d3.json('./src/data/garbage.csv', (data) => {
+      console.log(data)
+    })
+
+    // let chartData = d3.csv("/data/garbage.csv", (data) => {
+    //                   // if (error) throw error;
+    //                   console.log('data:', data);
+                      
+    //                   data.forEach((d) => {
+    //                     // let garbage = this.state.garbage.slice();
+    //                     // garbage.push(d);
+    //                     // this.setState({ garbage: garbage })
+    //                     console.log(d)
+    //                   })
+    //                   return data;
+    //                 })
+    // debugger
+        // console.log('this.state', this.state)
+        // width = 500,
+        // height = 300,
+        // margins = {left: 100, right: 100, top: 50, bottom: 50},
+        // // chart series,
+        // // field: is what field your data want to be selected
+        // // name: the name of the field that display in legend
+        // // color: what color is the line
+        // chartSeries = [
+        //   {
+        //     field: 'total',
+        //     name: 'Total',
+        //     color: '#ff7f0e'
+        //   }
+        // ],
+        // // your x accessor
+        // x = function(d) {
+        //   return parseDate(d.month);
+        // },
+        // xScale = 'time';
 
     return (
       <div className='container'>
         <h1>Hello World</h1>
         <img src={car} alt='awesome' className='annotate-image' />
-        <Stage width={700} height={700}>
-          <Layer>
-            <Rect
-              x={10} 
-              y={10} 
-              width={50} 
-              height={50}
-              fill={this.state.color}
-              shadowBlur={10}
-              onClick={this.handleClick}
-            />
-            <Line
-              points={[5, 70, 140, 23, 250, 60, 300, 20]}
-              stroke={this.state.lineColor}
-              strokeWidth={15}
-              lineCap={'round'}
-              lineJoin={'round'}
-              onClick={this.handleLineClick}
-            />
-          </Layer>
-        </Stage>
+        <ProgressArc
+          height={300}
+          width={300}
+          innerRadius={100}
+          outerRadius={110}
+          id="d3-arc"
+          backgroundColor="#e6e6e6"
+          foregroundColor="#00ff00"
+          percentComplete={0.3}
+        />      
       </div>
     );
   }
